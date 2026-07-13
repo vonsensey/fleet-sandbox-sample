55af9bf11ab873aaf7f51671f3d41bf101b5c655
Blackcube Fleet <fleet@blackcube.se>
2026-07-14 00:52:53 +0200

    task 98: round 1

--- greet.py
+++ greet.py
@@ -4,3 +4,17 @@ def greet(name=None, loud=False):
     else:
         msg = "Hello from the Fleet Sandbox!"
     return msg.upper() if loud else msg
+
+
+def main(argv=None):
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Fleet Sandbox greeting")
+    parser.add_argument("name", nargs="?", default=None, help="name to greet")
+    parser.add_argument("--loud", action="store_true", help="shout the greeting")
+    args = parser.parse_args(argv)
+    print(greet(args.name, loud=args.loud))
+
+
+if __name__ == "__main__":
+    main()
